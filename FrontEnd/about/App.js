722c16054e9ef0128721197759d2b0fdcbd536d9
Jayani-pathirana <66511990+Jayani-pathirana@users.noreply.github.com>
2021-03-09 11:08:11 +0530

    About Us Page
    Add text in about page

--- FrontEnd/about/App.js
+++ FrontEnd/about/App.js
@@ -3,6 +3,21 @@ import React from 'react';
 import { StyleSheet, Text, Button, View } from 'react-native';
 
 export default function App() {
-  
+  return (
+    <View style={styles.container}>
+      <Text>About Us</Text>
+      <Text > </Text>
+      <Text >My Vocal Buddy is a </Text>
+      <Text >personal assistant which</Text>
+      <Text >can identify Stuttering and</Text>
+      <Text >assista individuals to reduce</Text>
+      <Text >Stuttering. To help them cope with their lives a little</Text>
+      <Text >easiar and enjoyable for them and support the </Text>
+      <Text >people who are taking care</Text>
+      <Text >of them.</Text>
+      <StatusBar style="auto" />
+
+    </View>
+  );
 }
 
