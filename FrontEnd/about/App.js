d1853ed379d274040726164088e72ac3c4247db6
Jayani-pathirana <66511990+Jayani-pathirana@users.noreply.github.com>
2021-03-09 11:10:30 +0530

    About Us page
    Add style sheet

--- FrontEnd/about/App.js
+++ FrontEnd/about/App.js
@@ -5,19 +5,38 @@ import { StyleSheet, Text, Button, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>About Us</Text>
-      <Text > </Text>
-      <Text >My Vocal Buddy is a </Text>
-      <Text >personal assistant which</Text>
-      <Text >can identify Stuttering and</Text>
-      <Text >assista individuals to reduce</Text>
-      <Text >Stuttering. To help them cope with their lives a little</Text>
-      <Text >easiar and enjoyable for them and support the </Text>
-      <Text >people who are taking care</Text>
-      <Text >of them.</Text>
+      <Text style={[styles.setFontSize,styles.setColorWhite]}>About Us</Text>
+      <Text style={[styles.setFontSize2,styles.setColorBlue]}> </Text>
+      <Text style={[styles.setFontSize2,styles.setColorWhite]}>My Vocal Buddy is a </Text>
+      <Text style={[styles.setFontSize2,styles.setColorWhite]}>personal assistant which</Text>
+      <Text style={[styles.setFontSize2,styles.setColorWhite]}>can identify Stuttering and</Text>
+      <Text style={[styles.setFontSize2,styles.setColorWhite]}>assista individuals to reduce</Text>
+      <Text style={[styles.setFontSize2,styles.setColorWhite]}>Stuttering. To help them cope with their lives a little</Text>
+      <Text style={[styles.setFontSize2,styles.setColorWhite]}>easiar and enjoyable for them and support the </Text>
+      <Text style={[styles.setFontSize2,styles.setColorWhite]}>people who are taking care</Text>
+      <Text style={[styles.setFontSize2,styles.setColorWhite]}>of them.</Text>
       <StatusBar style="auto" />
 
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    fontSize: 34,
+    backgroundColor: '#270264',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  setFontSize: {
+    fontSize: 40,
+    fontWeight : 'bold' 
+  },
+  setFontSize2: {
+    fontSize: 20 
+  },
+  setColorWhite : {
+    color: '#ffffff'
+  },
+});
