0368e0eb56bd5488092570cc3b1a2d7c6ff8d374
Jayani-pathirana <66511990+Jayani-pathirana@users.noreply.github.com>
2021-04-09 20:18:40 +0530

    style

--- MyVocalBuddy/src/views/sign-up/SignUp.js
+++ MyVocalBuddy/src/views/sign-up/SignUp.js
@@ -94,6 +94,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
+        backgroundColor: '#270264',
         alignItems: 'center'
     }
 })
