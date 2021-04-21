2bc89ec4759946d45f8f082324d164a2295c5e69
Ieshitha Wijetunge <66556395+Ieshitha@users.noreply.github.com>
2021-04-21 23:41:05 +0530

    implement fetch to retreive the output

--- MyVocalBuddy/App.js
+++ MyVocalBuddy/App.js
@@ -8,6 +8,12 @@ import LoadingScreen from './src/views/loading/loading';
 import TutorialScreen from './src/views/tutorial/tutorial';
 import SignUpScreen from './src/views/sign-up/SignUp';
 import 'react-native-gesture-handler';
+import LoadingScreen from './src/views/Loading/loading';
+import TuteOneScreen from './src/views/tuteOne/tuteOne';
+import TuteTwoScreen from './src/views/tuteTwo/tuteTwo';
+import TuteThreeScreen from './src/views/tuteThree/tuteThree';
+import TuteFourScreen from  './src/views/tuteFour/tuteFour';
+import TuteFiveScreen from  './src/views/tuteFive/tuteFive';
 // import { createStackNavigator, createAppContainer } from "react-navigation";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
@@ -18,13 +24,18 @@ export default function App() {
   return (
    
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignUp">
+     <Stack.Navigator initialRouteName="Tutorials">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="About" component={AboutScreen} />
         <Stack.Screen name="SignIn" component={SignInScreen} />
         <Stack.Screen name="Loading" component={LoadingScreen}/>
         <Stack.Screen name="Tutorials" component={TutorialScreen}/>
         <Stack.Screen name="SignUp" component={SignUpScreen}/>
+        <Stack.Screen name="TuteOne" component={TuteOneScreen} />
+        <Stack.Screen name="TuteTwo" component={TuteTwoScreen} />
+        <Stack.Screen name="TuteThree" component={TuteThreeScreen} />
+        <Stack.Screen name="TuteFour" component={TuteFourScreen} />
+        <Stack.Screen name="TuteFive" component={TuteFiveScreen} />
       </Stack.Navigator>
     </NavigationContainer>
     
