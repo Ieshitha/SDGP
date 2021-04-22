7d060493b8e2700e3a3a8d60865539789366d9cd
Ieshitha Wijetunge <66556395+Ieshitha@users.noreply.github.com>
2021-04-22 13:17:40 +0530

    added AWS url

--- MyVocalBuddy/src/util/Constants.js
+++ MyVocalBuddy/src/util/Constants.js
@@ -1,1 +1,2 @@
-export const APP_DOMAIN = "http://10.0.2.2:5000/"
+// export const APP_DOMAIN = "http://10.0.2.2:5000/"
+export const APP_DOMAIN ="http://ec2-15-206-90-1.ap-south-1.compute.amazonaws.com:8080/"
